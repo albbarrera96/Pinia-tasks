947df0bcddb468a52ddfb44f1a4ed6c3bf90e23d
albbarrera96 <85797748+albbarrera96@users.noreply.github.com>
2022-10-13 16:58:21 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+    <main style="background:teal">
+        <div class="main shadow-sm">
+            <div class="text-center py-3">
+                <h1>Pinia Tasks</h1>
+            </div>
+        </div>
+        <div class="container-xl" id="container">
+            <h1>HELLO</h1>
+        </div>
+    </main>
+
+</template>
+
+<style scoped>
+.main{
+    background: rgb(179, 179, 179);
+}
+
+
+</style>
